d2ab265a80dc257437c3e2db26b9200d87f3fb76
T K <gktomo@gmail.com>
2024-02-26 23:08:01 +0900

    Revert "テストがすべて成功するように修正"
    This reverts commit fd681201422f1a00a588d2e891411510acdedf0d.

--- tests/test_say.py
+++ tests/test_say.py
@@ -4,8 +4,7 @@ def test_foo_say():
     assert Foo().say() == 'foo'
 
 def test_foo_say2():
-    # assert Foo().say2() == 'foo'  #テストエラーの出力確認。
-    assert Foo().say2() == 'foo2'
+    assert Foo().say2() == 'foo'  #テストエラーの出力確認。
 
 def test_hoge_say():
     assert Hoge().say() == 'hoge'
